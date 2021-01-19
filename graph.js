cce76b5c0ec68bc8dfd4de1032653e98292c1f55
Saif <saifo1999@gmail.com>
2021-01-19 06:52:48 +0200

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,16 +1,18 @@
 class LLNode {
     value;
     next;
-    constructor(v, n) {
+    weight;
+    constructor(v, n, w = "") {
         this.value = v;
         this.next = n;
+        this.weight = w;
     }
 }
 
 function bfs(v, s) {
-    let color = Array(v.length), d = Array(v.length)
-        , p = Array(v.length), queue = Array();
-    for (let i = 0; i < v.length; i++)// O(|V|)
+    let color = Array(), d = Array()
+        , p = Array(), queue = Array();
+    for (let i in v)// O(|V|)
         color[i] = "white", d[i] = -1, p[i] = null;
     color[s] = "gray", d[s] = 0;
     queue.push(s)
@@ -32,14 +34,18 @@ function bfs(v, s) {
     return [d, p];
 }// O(|V| + |E|)
 
-let v = new Array(7);
-v[0] = new LLNode(1, new LLNode(3, null));
-v[1] = new LLNode(0, new LLNode(3, new LLNode(2, new LLNode(6, new LLNode(5, null)))));
-v[2] = new LLNode(1, new LLNode(3, new LLNode(4, new LLNode(5, null))));
-v[3] = new LLNode(0, new LLNode(1, new LLNode(2, new LLNode(4, null))));
-v[4] = new LLNode(2, new LLNode(3, new LLNode(6, null)));
-v[5] = new LLNode(1, new LLNode(2, null))
-v[6] = new LLNode(1, new LLNode(4, null))
+let v = new Array();
+v[1] = new LLNode(2, new LLNode(5, null));
+v[2] = new LLNode(1, new LLNode(5, new LLNode(4, new LLNode(3, null))));
+v[3] = new LLNode(2, new LLNode(4, null));
+v[4] = new LLNode(3, new LLNode(2, new LLNode(5, new LLNode(6, null))));
+v[5] = new LLNode(1, new LLNode(2, new LLNode(4, new LLNode(7, null))))
+v[6] = new LLNode(4, new LLNode(11, new LLNode(12, new LLNode(9, null))))
+v[7] = new LLNode(5, new LLNode(9, new LLNode(8, null)));
+v[8] = new LLNode(7, new LLNode(9, null));
+v[9] = new LLNode(6, new LLNode(7, new LLNode(8, null)));
+v[11] = new LLNode(12, new LLNode(6, null));
+v[12] = new LLNode(11, new LLNode(6, null));
 // sum of length of all linked lists = 2|E| (undirected graph)
 
 let [distance, predecessor] = bfs(v, 0)
