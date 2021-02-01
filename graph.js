82e6332393d7c872de175b07b21f6c468f5f6a8e
Saif <saifo1999@gmail.com>
2021-02-01 10:04:12 +0200

    add dfs

--- graph.js
+++ graph.js
@@ -36,18 +36,48 @@ function bfs(v, s) {
 
 let v = new Array();
 v[1] = new LLNode(2, new LLNode(5, null));
-v[2] = new LLNode(1, new LLNode(5, new LLNode(4, new LLNode(3, null))));
+v[2] = new LLNode(1, new LLNode(3, new LLNode(4, new LLNode(5, null))));
 v[3] = new LLNode(2, new LLNode(4, null));
-v[4] = new LLNode(3, new LLNode(2, new LLNode(5, new LLNode(6, null))));
-v[5] = new LLNode(1, new LLNode(2, new LLNode(4, new LLNode(7, null))))
-v[6] = new LLNode(4, new LLNode(11, new LLNode(12, new LLNode(9, null))))
-v[7] = new LLNode(5, new LLNode(9, new LLNode(8, null)));
-v[8] = new LLNode(7, new LLNode(9, null));
-v[9] = new LLNode(6, new LLNode(7, new LLNode(8, null)));
-v[11] = new LLNode(12, new LLNode(6, null));
-v[12] = new LLNode(11, new LLNode(6, null));
+v[4] = new LLNode(2, new LLNode(3, new LLNode(5, null)));
+v[5] = new LLNode(1, new LLNode(2, new LLNode(4, null)));
+// v[6] = new LLNode(4, new LLNode(11, new LLNode(12, new LLNode(9, null))))
+// v[7] = new LLNode(5, new LLNode(9, new LLNode(8, null)));
+// v[8] = new LLNode(7, new LLNode(9, null));
+// v[9] = new LLNode(6, new LLNode(7, new LLNode(8, null)));
+// v[11] = new LLNode(12, new LLNode(6, null));
+// v[12] = new LLNode(11, new LLNode(6, null));
 // sum of length of all linked lists = 2|E| (undirected graph)
 
-let [distance, predecessor] = bfs(v, 1)
+let [distance, predecessor] = bfs(v, 4)
 
 console.log({ distance, predecessor })
+
+// dfs
+// let color = Array(), p = Array(), d = Array(), f = Array(), time = 0
+
+function dfs(v) {
+    color = Array(), p = Array(), d = Array(), f = Array();
+    for (let i in v)// O(|V|)
+        color[i] = "white", p[i] = null, d[i] = -1, f[i] = -1;
+    time = 0
+    for (let i in v) { // O(|V|) without counting dfs_visit
+        if (color[i] === "white")
+            dfs_visit(v, i)
+    }
+}
+
+function dfs_visit(v, u) {
+    color[u] = "gray"
+    d[u] = ++time
+    let ll = v[u]
+    while (ll != null) {
+        let w = ll.value;
+        ll = ll.next;
+        if (color[w] !== "white")
+            continue;
+        p[w] = u
+        dfs_visit(v, w)
+    }
+    color[u] = "black"
+    f[u] = ++time
+}// O(|V| + |E|)
